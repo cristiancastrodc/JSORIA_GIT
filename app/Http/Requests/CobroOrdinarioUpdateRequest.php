1762d4d64773e12ea98eb0af78f5e0a4dc761df6
Bárbara Hari Galarreta Zuñiga <barbaragalarreta@gmail.com>
2016-02-21 05:49:23 -0500

    Actualizacion del request

--- app/Http/Requests/CobroOrdinarioUpdateRequest.php
+++ app/Http/Requests/CobroOrdinarioUpdateRequest.php
@@ -13,7 +13,7 @@ class CobroOrdinarioUpdateRequest extends Request
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class CobroOrdinarioUpdateRequest extends Request
     public function rules()
     {
         return [
-            //
+            'nombre' => 'required|string',
+            'monto' => 'required|numeric',
+            'unitario' => 'required|in:true,false',
+            'estado' => 'required|in:true,false',
         ];
     }
 }
