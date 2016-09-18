ac9577a09835b2251b3aa062627d398c2650d931
Cristian Castro <cristiancastrodc@gmail.com>
2016-09-17 22:31:53 -0500

    Actualizacion de script para restaurar DB

--- db/restaurar_db.sql
+++ db/restaurar_db.sql
@@ -1,3 +1,12 @@
+/********** TABLA INSTITUCION **********/
+TRUNCATE jsoria_institucion;
+INSERT INTO jsoria_institucion(nombre, id_razon_social, razon_social, ruc, direccion)
+VALUES ('Institución Educativa J. Soria', '1', 'Corporación Educativa J-Soria S.C.R.LTDA', '20490041339', 'Jr. Quillabamba N° 110 - Quillabamba'),
+       ('CEBA Konrad Adenahuer', '1', 'Corporación Educativa J-Soria S.C.R.LTDA', '20490041339', 'Jr. Quillabamba N° 110 - Quillabamba'),
+       ('Instituto Superior Tecnológico Urusayhua', '1', 'Corporación Educativa J-Soria S.C.R.LTDA', '20490041339', 'Jr. Quillabamba N° 110 - Quillabamba'),
+       ('Universidad Líder Peruana', '2', 'Universidad Privada Líder Peruana S.A.C.', '20564356035', 'Jr. Quillabamba N° 110 - Quillabamba');
+
+
 - Institucion: Todo
 - Detalle Institucion: Todo
 - Grado: Todo
