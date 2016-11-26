03adde8514317a146d4b8c5a94931c44a567d185
Bárbara Hari Galarreta Zuñiga <barbaragalarreta@gmail.com>
2016-11-26 09:41:00 -0500

    reportes secretaria

--- app/Http/Controllers/ReportesSecretariaController.php
+++ app/Http/Controllers/ReportesSecretariaController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use JSoria\Http\Requests;
 use JSoria\Http\Controllers\Controller;
 use JSoria\Deuda_Ingreso;
+use JSoria\Alumno;
 use JSoria\Usuario_Modulos;
 
 class ReportesSecretariaController extends Controller
@@ -106,4 +107,57 @@ class ReportesSecretariaController extends Controller
         $deudas = Deuda_Ingreso::deudasPorGrado($request['grado']);
         return 'hola';
     }
-}
+    /**
+     * Recuperar los períodos del alumno
+     */
+    public function periodosAlumno($nro_documento)
+    {
+      $alumno = Alumno::datosAlumno($nro_documento);
+      $periodos = Alumno::periodosAlumno($nro_documento);
+      $respuesta = array('alumno' => $alumno, 'periodos' => $periodos);
+      return $respuesta;
+    }
+    /**
+     * Mostrar la pantalla de Lista de Ingresos por Cajera
+     */
+    public function cuentaDeAlumno()
+    {
+      $modulos = Usuario_Modulos::modulosDeUsuario();
+      return view('secretaria.reportes.index',
+        ['modulos' => $modulos]
+      );
+    }
+    /**
+     * Mostrar la pantalla de Lista de Ingresos por Cajera
+     */
+    public function procesarCuentaDeAlumno(Request $request)
+    {
+      $nro_documento = $request->nro_documento;
+      $id_categoria = $request->id_categoria;
+      $tipo_reporte = $request->tipo_reporte;
+      // Recuperar valores enviados y de la base de datos
+      $fecha_archivo = date('d-m-Y H:i:s');
+      $archivo = 'Reporte de Cuenta de Alumno -' . $nro_documento . '-' . $fecha_archivo;
+      $cuenta = Alumno::cuentaAlumno($nro_documento, $id_categoria);
+      $alumno = Alumno::datosAlumno($nro_documento);
+      $fecha = date('d-m-Y');
+      // Generar el PDF
+      if ($tipo_reporte == 'pdf') {
+        $view = \View::make('admin.reportes.cuenta_alumno_rept',
+          ['cuenta' => $cuenta, 'alumno' => $alumno, 'fecha' => $fecha]
+        )->render();
+        $pdf = \App::make('dompdf.wrapper');
+        $pdf->loadHTML($view);
+        return $pdf->stream($archivo);
+      } else {
+        \Excel::create($archivo, function($excel) use ($cuenta, $alumno, $fecha) {
+          $excel->sheet('Hoja 1', function($sheet) use ($cuenta, $alumno, $fecha) {
+            $sheet->loadView('admin.reportes.cuenta_alumno_rept', array(
+              'cuenta' => $cuenta,
+              'alumno' => $alumno,
+              'fecha' => $fecha,
+            ));
+          });
+        })->download('xls');
+      }
+    }}
--- resources/views/secretaria/reportes/deudas.blade.php
+++ resources/views/secretaria/reportes/deudas.blade.php
@@ -1,59 +1,76 @@
 @extends('layouts.dashboard')
 
 @section('title')
-  Reporte de Deudas por Grado
+  Reporte de Alumnos Deudores
 @endsection
 
 @section('content')
+
   @if(Session::has('message'))
-  <div class="row">
-    <div class="col-sm-10">
-      <div class="alert alert-success alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        {{Session::get('message')}}
-      </div>
+    <div class="alert alert-success alert-dismissible" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      {{Session::get('message')}}
     </div>
-  </div>
   @endif
 
   @include('messages.errors')
 
   <div class="row">
-    <div class="col-md-10">
+    <div class="col-md-8">
       <div class="card hoverable">
         <div class="card-header main-color ch-alt">
-          <h2>Deudas por Grados</h2>
+          <h2>Reporte de Alumnos deudores</h2>
         </div>
         <div class="card-body card-padding">
-          <form action="{{ url('/secretaria/procesar/reporte/deudas_por_grado') }}" class="form-horizontal">
+          <form action="{{ url('/admin/reportes/AlumnosDeudores/procesar') }}" method="POST" class="form-horizontal" id="form-listar_deudores">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <div class="form-group">
               <label for="id_institucion" class="col-sm-3 control-label">Institución</label>
               <div class="col-sm-9">
-                <select class="selectpicker" name="id_institucion" id="id_institucion" title='Seleccione'>
+                <select class="selectpicker" name="id_institucion" id="id_institucion" title='Seleccione Institución'>
                   <option value="1">I.E. J. Soria</option>
                   <option value="2">CEBA Konrad Adenahuer</option>
                   <option value="3">I.S.T. Urusayhua</option>
-                  <option value="4">ULP</option>
+                  <option value="4">Universidad Privada Líder Peruana</option>
                 </select>
               </div>
             </div>
             <div class="form-group">
               <label for="nombre_division" class="col-sm-3 control-label"></label>
               <div class="col-sm-9">
-                <select class="selectpicker" name="id_detalle_institucion" id="id_detalle_institucion" title='Seleccione'>
+                <select class="selectpicker" name="id_detalle_institucion" id="id_detalle_institucion" title='Seleccione Nivel o Carrera'>
                 </select>
               </div>
             </div>
             <div class="form-group">
-              <label for="grado" class="col-sm-3 control-label">Grado</label>
+              <label for="grado" class="col-sm-3 control-label"></label>
               <div class="col-sm-9">
-                <select class="selectpicker" name="grado" id="grado" title='Seleccione'>
+                <select class="selectpicker" name="grado" id="grado" title='Seleccione Grado o Semestre'>
                 </select>
               </div>
             </div>
+            <!-- EXCEL O PDF -->
+            <div class="form-group">
+              <label for="tipo_reporte" class="control-label col-sm-3">Tipo de Reporte</label>
+              <div class="col-sm-9">
+                <div class="radio">
+                  <label>
+                      <input type="radio" name="tipo_reporte" value="pdf" checked="checked">
+                      <i class="input-helper"></i>PDF
+                  </label>
+                </div>
+                <div class="radio">
+                  <label>
+                      <input type="radio" name="tipo_reporte" value="excel">
+                      <i class="input-helper"></i>Excel
+                  </label>
+                </div>
+              </div>
+            </div>
+            <!--/ EXCEL O PDF -->
             <div class="form-group">
               <div class="col-sm-3 col-sm-offset-9">
-                <button type="submit" class="btn btn-block waves-effect m-t-15 accent-color" formtarget="_blank">Procesar</button>
+                <button type="submit" class="btn btn-warning waves-effect btn-block" id="btn-reporte-ListarIngreso" formtarget="_blank">Generar</button>
               </div>
             </div>
           </form>
@@ -61,4 +78,8 @@
       </div>
     </div>
   </div>
+@endsection
+
+@section('scripts')
+  <script src="{{ asset('js/reportes.js') }}"></script>
 @endsection
